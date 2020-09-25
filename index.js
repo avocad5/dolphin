c5874bcca467eea02b48a7fb973db54560f12c7e
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-09-25 09:53:06 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function getblockid(name){
+ return _.find(Entry.variableContainer.functions_,
+        d => d.block.template.replace(/ /gi,'') == name).id
+}
+
+Entry.block[`func_${getblockid('%1노래재생%2')}`].paramsKeyMap = { content: 0 };
+Entry.block[`func_${getblockid('%1노래재생%2')}`].func = (sprite, script) => {
+  window.open(Entry.variableContainer.getListByName('링크').array_[Number(script.getValue('content',script))-1])
+}
