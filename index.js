7ef10aa1145dc2be978d60e5d828cb75c9c82957
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-09-26 12:44:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function getblockid(name){
 
 Entry.block[`func_${getblockid('%1노래재생%2')}`].paramsKeyMap = { content: 0 };
 Entry.block[`func_${getblockid('%1노래재생%2')}`].func = (sprite, script) => {
+  Entry.variableContainer.getVariableByName('노래번호').value_ = script.getValue('content',script)
   window.open(Entry.variableContainer.getListByName('링크').array_[Number(script.getValue('content',script))-1].data)
   Entry.variableContainer.getListByName('인기도').array_[Number(script.getValue('content',script))-1].data = Number(Entry.variableContainer.getListByName('인기도').array_[Number(script.getValue('content',script))-1].data)+1
 }
